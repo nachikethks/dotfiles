09a2c3492ff2ff3d2245d2abcf5ce9bbe199dc50
Nachiketh K S <nachikethks2000@gmail.com>
2026-03-03 07:27:14 +0530

    improvements

--- zsh/.config/zsh/conf.d/50-aliases.zsh
+++ zsh/.config/zsh/conf.d/50-aliases.zsh
@@ -14,6 +14,7 @@ alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 alias -- -='cd -'
+alias gs='git status'
 
 alias openvpn_connect="sudo openvpn ~/Downloads/aihub-qa.ovpn"
 alias claude-mem="$HOME/.bun/bin/bun \"$HOME/.claude/plugins/marketplaces/thedotmack/plugin/scripts/worker-service.cjs\""
