71a00da8261238fabdba0bb8f5384ca1b034a2ed
Nachiketh K S <nachikethks2000@gmail.com>
2026-03-06 16:42:39 +0530

    [tmux] fix: duplicate config

--- zsh/.config/zsh/conf.d/40-keybindings.zsh
+++ zsh/.config/zsh/conf.d/40-keybindings.zsh
@@ -1,6 +1,8 @@
 bindkey '^r' history-incremental-search-backward
 bindkey '^p' history-search-backward
 bindkey '^n' history-search-forward
+bindkey '^k' history-search-backward
+bindkey '^j' history-search-forward
 bindkey '^I' complete-word
 
 # Accept autosuggestion (full line)
