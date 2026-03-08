d1ae00f8d849729986fffa7cf8e50010b5c3f677
Nachiketh K S <nachikethks2000@gmail.com>
2026-03-08 08:14:13 +0530

    feat(nvim): Add <leader>gd/gr to flash jump to definition/references

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,22 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Flash jump + LSP actions
+vim.keymap.set("n", "<leader>gd", function()
+  require("flash").jump({
+    action = function(match)
+      vim.api.nvim_win_set_cursor(0, { match.pos[1], match.pos[2] })
+      vim.lsp.buf.definition()
+    end,
+  })
+end, { desc = "Flash → Go to Definition" })
+
+vim.keymap.set("n", "<leader>gr", function()
+  require("flash").jump({
+    action = function(match)
+      vim.api.nvim_win_set_cursor(0, { match.pos[1], match.pos[2] })
+      Snacks.picker.lsp_references()
+    end,
+  })
+end, { desc = "Flash → Go to References" })
