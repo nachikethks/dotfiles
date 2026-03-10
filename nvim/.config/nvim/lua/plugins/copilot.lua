f509aa2bd63084020f201cc96d04e49711026a58
Nachiketh K S <nachikethks2000@gmail.com>
2026-03-10 10:13:06 +0530

    feat(nvim): Add copilot lsp with NES

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -16,7 +16,7 @@ return {
       vim.lsp.enable("copilot_ls")
 
       -- Accept NES suggestion in normal mode with Tab
-      vim.keymap.set("n", "<tab>", function()
+      local function accept_nes_or_fallback()
         local nes = require("copilot-lsp.nes")
         local state = vim.b[vim.api.nvim_get_current_buf()].nes_state
         if state then
@@ -26,7 +26,28 @@ return {
         else
           return "<C-i>"
         end
-      end, { expr = true, desc = "Accept Copilot NES" })
+      end
+
+      local function accept_nes_insert_or_fallback()
+        local ok, suggestion = pcall(require, "copilot.suggestion")
+        if ok and suggestion.is_visible() then
+          suggestion.accept()
+          return ""
+        end
+
+        local nes = require("copilot-lsp.nes")
+        local state = vim.b[vim.api.nvim_get_current_buf()].nes_state
+        if state then
+          local _ = nes.walk_cursor_start_edit()
+            or (nes.apply_pending_nes() and nes.walk_cursor_end_edit())
+          return ""
+        end
+
+        return "<S-Tab>"
+      end
+
+      vim.keymap.set("n", "<tab>", accept_nes_or_fallback, { expr = true, desc = "Accept Copilot NES" })
+      vim.keymap.set("i", "<S-Tab>", accept_nes_insert_or_fallback, { expr = true, desc = "Accept Copilot NES" })
     end,
     opts = {
       nes = {
--- nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,6 +1,15 @@
 return {
   { "mason-org/mason-lspconfig.nvim", opts = { ensure_installed = { "ruff" } } },
   {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      if not vim.tbl_contains(opts.ensure_installed, "copilot-language-server") then
+        table.insert(opts.ensure_installed, "copilot-language-server")
+      end
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
