e47454d092ffb70861de2b3514eb225dabc6ad9d
Nachiketh K S <nachikethks2000@gmail.com>
2026-03-21 20:18:44 +0530

    feat(fish): yank to system clipboard in vi mode

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -15,6 +15,10 @@ function fish_user_key_bindings
     bind -M insert \ck history-search-backward
     bind -M insert \cj history-search-forward
 
+    # Yank to system clipboard in vi mode
+    bind -M visual y 'fish_clipboard_copy; commandline -f end-selection'
+    bind -M normal yy fish_clipboard_copy
+
     # Re-apply autopair bindings after vi mode setup
     functions -q _autopair_fish_key_bindings && _autopair_fish_key_bindings
 end
