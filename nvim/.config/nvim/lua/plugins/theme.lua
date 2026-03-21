b750a4841258140a815d904a7f497e58f8689b62
Nachiketh K S <nachikethks2000@gmail.com>
2026-03-21 19:37:39 +0530

    feat(kitty,nvim): enable transparency for kitty and nvim

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,5 +1,15 @@
 return {
   {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      transparent_background = true,
+      float = {
+        transparent = true,
+      },
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin-mocha",
